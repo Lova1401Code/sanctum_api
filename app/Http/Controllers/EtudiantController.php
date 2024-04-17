44806609fc6eeb7e44bcee96d5221904d4448674
Lova Ramih <lova.ramiharisoa@gmail.com>
2024-04-17 19:21:01 +0300

    mise en place login, logout, profil

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Etudiant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class EtudiantController extends Controller
@@ -13,7 +14,7 @@ class EtudiantController extends Controller
     public function register(Request $request) {
         //validation
         $request->validate([
-            'name' => "required",
+            'name' => "required|min:8",
             'email' => "required|email|unique:etudiants",
             'password' => "required|confirmed"
         ]);
@@ -33,13 +34,56 @@ class EtudiantController extends Controller
         ]);
 
     }
+    //Authentification de l'utilisateur
     public function login(Request $request) {
-        
+        //validation
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        //verification si l'etudiant existe
+        //URL : http://127.0.0.1:8000/api/login
+        $etudiant = Etudiant::where('email', '=', $request->email)->first(); 
+        if ($etudiant) {
+            if (Hash::check($request->password, $etudiant->password)) {
+                //creer un jeton/token
+                $token = $etudiant->createToken('auth_tokenEtudiant')->plainTextToken;
+                //réponse pour envoyer le token
+                return response()->json([
+                    "status" => 1,
+                    "message" => "connexion réussie",
+                    "access_token" => $token 
+                ]);
+            }else{
+                return response()->json([
+                    "status" => 0,
+                    "message" => "mot de passe incorrect"
+                ]);
+            }
+        }else{
+            return response()->json([
+                "status" => 0,
+                "message" => "Etudiant introuvable"
+            ], 404);
+        }
     }
+    //déconnexion
+    //URL : http://127.0.0.1:8000/api/logout
     public function logout(Request $request) {
-        
+        Auth::user()->tokens()->delete();
+        return response()->json([
+            "status" => 1,
+            "message" => "Deconnexion réussie"
+        ]);
     }
-    public function profil(Request $request) {
-        
+    //récupération de profil d'un utilisateur
+    //URL : http://127.0.0.1:8000/api/profil
+    public function profil() {
+        return response()->json([
+            "status" => 1,
+            "message" => "Information profile",
+            "datas" => Auth::user()
+        ]);
     }
 }

bf368eaa79e157a3ace94588c5c55cdfd6baeaf2
Lova Ramih <lova.ramiharisoa@gmail.com>
2024-04-17 14:29:47 +0300

    définit le route de notre api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\EtudiantController;
+use App\Http\Controllers\ProjetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//Routes du controlleur Etudiant
+Route::post("register", [EtudiantController::class, 'register']);
+Route::post("login", [EtudiantController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    //etudiant setting
+    Route::get("profil", [EtudiantController::class, 'profil']);
+    Route::get("logout", [EtudiantController::class, 'logout']);
+
+    //projet manipulation
+    Route::post("create", [ProjetController::class, 'create']);
+    Route::get('detailProjet', [ProjetController::class, 'details']);
+    Route::get('listeProjet', [ProjetController::class, 'liste']);
+    Route::delete('delete', [ProjetController::class, 'delete']);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();

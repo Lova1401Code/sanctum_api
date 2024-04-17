17a52c314f53da43b782a5c6c4e581e9bdb65b63
Lova Ramih <lova.ramiharisoa@gmail.com>
2024-04-17 15:14:16 +0300

    création du méthode register pour enregistrer un étudiant

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Etudiant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class EtudiantController extends Controller
 {
+    //création d'un étudiant
+    //URL: http://127.0.0.1:8000/api/register
     public function register(Request $request) {
-        
+        //validation
+        $request->validate([
+            'name' => "required",
+            'email' => "required|email|unique:etudiants",
+            'password' => "required|confirmed"
+        ]);
+
+        //traitement des données
+        $etudiant = new Etudiant();
+        $etudiant->name = $request->name;
+        $etudiant->email = $request->email;
+        $etudiant->password = Hash::make($request->password);
+        $etudiant->phone = isset($request->phone) ? $request->phone : '';
+        $etudiant->save();
+
+        //réponse
+        return response()->json([
+            "status" => 1,
+            "message" => "étudiant crée avec success"
+        ]);
+
     }
     public function login(Request $request) {
         
